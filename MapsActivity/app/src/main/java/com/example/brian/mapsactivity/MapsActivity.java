90928f7797688bdf52860549db773c9c825aea9d
sarkotik <brianyuliu@gmail.com>
2017-05-30 10:39:55 -0700

    This is Brian Liu. I added the current location tracker and added a button to change the views of the map. I then followed along in class and I'm working on the LocaitonListener portions of the app.
    This is Brian Liu. I added the current location tracker and added a
    button to change the views of the map. I then followed along in class
    and I'm working on the LocaitonListener portions of the app.

--- MapsActivity/app/src/main/java/com/example/brian/mapsactivity/MapsActivity.java
+++ MapsActivity/app/src/main/java/com/example/brian/mapsactivity/MapsActivity.java
@@ -1,11 +1,17 @@
 package com.example.brian.mapsactivity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,6 +24,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private boolean isGPSEnabled = false;
+    private boolean isNetworkEnabled = false;
+    private boolean canGetLocation = false;
+    private static final long MIN_TIME_BW_UPDATES = 1000*15;
+    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 5.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
+
 
 
     /**
@@ -43,11 +57,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
         //double latitude = location.getLatitude();
         //double longitude = location.getLongitude();
-
-
-
 
 
         // Add a marker in Sydney and move the camera
@@ -56,8 +68,79 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(birthPlace).title("Born Here!!!!"));
         //mMap.addMarker(new MarkerOptions().position(currentPos).title("Current Pos!!!!"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(birthPlace));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("MapsApp", "Permission check 1 failed");
+            Log.d("MapsApp", Integer.toString(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)));
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("MapsApp", "Failed Permission check 2");
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+        }
+
+        mMap.setMyLocationEnabled(true);
+
     }
+
+    public void getLocation() {
+        try{
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            //get GPS status
+            isGPSEnabled  = locationManager.isProviderEnabled(locationManager.GPS_PROVIDER);
+            if(isGPSEnabled) Log.d("MyMaps", "getLocation: GPS is enabled");
+            //get Network status
+            isNetworkEnabled = locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER);
+            if(isNetworkEnabled) Log.d("MyMaps", "getLocation: Network is enabled");
+
+            if(!isGPSEnabled && !isNetworkEnabled)
+            {
+                Log.d("MyMaps", "getLocation: No Provider is enabled!!");
+            }else{
+                this.canGetLocation = true;
+
+                if(isGPSEnabled){
+                    Log.d("MyMap", "getLocation : GPS Enabled - requesting location update");
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                            locationListenerGps);
+                    Log.d("MyMap", "getLocation : Network GPS update request successful");
+                    Toast.makeText(this, "Using GPS", Toast.LENGTH_SHORT);
+                }
+                if(isNetworkEnabled){
+                    Log.d("MyMap", "getLocation : Network Enabled - requesting location update");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                            locationListenerNetwork);
+                    Log.d("MyMap", "getLocation : Network GPS update request successful");
+                    Toast.makeText(this, "Using GPS", Toast.LENGTH_SHORT);
+                }
+            }
+
+        } catch(Exception e){
+            Log.d("MyMaps", "Caught an exception in my getLocation");
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+    public void changeMapType(View v) {
+        if (mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        } else if (mMap.getMapType() == GoogleMap.MAP_TYPE_SATELLITE) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+    }
+
+
 }
+
 
 
 
